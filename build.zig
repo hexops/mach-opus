52560199cd9df78cfdb52c6fafd91717730b1166
Ali Chraghi <alichraghi@proton.me>
2023-08-12 12:23:20 +0330

    build: restore test step

--- build.zig
+++ build.zig
@@ -8,6 +8,17 @@ pub fn build(b: *std.Build) void {
     const optimize = b.standardOptimizeOption(.{});
     const libopusfile_dep = b.dependency("opusfile", .{ .target = target, .optimize = optimize });
 
+    const main_test = b.addTest(.{
+        .root_source_file = .{ .path = "src/lib.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
+    main_test.linkLibrary(libopusfile_dep.artifact("opusfile"));
+    b.installArtifact(main_test);
+
+    const test_step = b.step("test", "Run library tests");
+    test_step.dependOn(&b.addRunArtifact(main_test).step);
+
     const example = b.addExecutable(.{
         .name = "example-play",
         .root_source_file = .{ .path = "examples/play.zig" },
