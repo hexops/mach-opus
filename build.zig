58a22201881d7639339f3c029d13a00bbfe005e2
Stephen Gutekanst <stephen@hexops.com>
2024-03-08 20:41:38 -0700

    remove example build logic
    Signed-off-by: Stephen Gutekanst <stephen@hexops.com>

--- build.zig
+++ build.zig
@@ -29,19 +29,4 @@ pub fn build(b: *std.Build) void {
 
     const test_step = b.step("test", "Run library tests");
     test_step.dependOn(&b.addRunArtifact(main_tests).step);
-
-    const example = b.addExecutable(.{
-        .name = "example-play",
-        .root_source_file = .{ .path = "examples/play.zig" },
-        .target = target,
-        .optimize = optimize,
-    });
-    example.root_module.addImport("mach-opus", module);
-    b.installArtifact(example);
-
-    const example_run_cmd = b.addRunArtifact(example);
-    example_run_cmd.step.dependOn(b.getInstallStep());
-
-    const example_run_step = b.step("run-example", "Run example");
-    example_run_step.dependOn(&example_run_cmd.step);
 }
